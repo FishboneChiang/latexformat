7d63fe0ebe32cefa07d6f60680147788434a5f9a
Fishbone Chiang <fishbone99999999@gmail.com>
2026-05-31 07:39:26 +0800

    inline math

--- latexformat.nim
+++ latexformat.nim
@@ -8,11 +8,16 @@ proc isEscaped(s: string, pos: int): bool =
 proc left_right_pairs(line: string): (int, int) =
   var left: int = 0
   var right: int = 0
+  var in_math = false
   for i in 0..<(line.len):
+    if line[i] == '$' and (i == 0 or not isEscaped(line, i)):
+      in_math = not in_math
+      continue
+    if in_math: continue
     if line[i] == '{' or line[i] == '[':
       if i > 0 and isEscaped(line, i): continue
       left += 1
-    if line[i] == '}' or line[i] == ']':
+    elif line[i] == '}' or line[i] == ']':
       if i > 0 and isEscaped(line, i): continue
       right += 1
   return (left, right)
